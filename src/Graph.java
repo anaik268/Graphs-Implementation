91e2434f7936ae910ce5a4b0163bac8752459171
Abhishek Naik <an286@njit.edu>
2016-03-21 09:39:57 -0400

    Added comment citation to original author

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,12 @@
+/* Note:
+ * 
+ * This code was not written by me and I do not take any credit. I had simply typed it up so I can 
+ * better understand how graphs work. 
+ * 
+ * This was written by Sesh Venugopal, a professor from Rutgers. You can watch his video at the following link:
+ * https://www.youtube.com/watch?v=zVrPdF7f4-I
+ */
+
 import java.util.*;
 import java.io.*;
 
